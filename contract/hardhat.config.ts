54136da83cae439d8b669ec912236e31d5e949fe
JACOB OLUWAFEMI <167685170+Holuwaphemmy01@users.noreply.github.com>
2025-10-06 10:30:43 +0100

    update on hardhat config file

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,41 +1,69 @@
-import type { HardhatUserConfig } from "hardhat/config";
+// import type { HardhatUserConfig } from "hardhat/config";
 
-import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { configVariable } from "hardhat/config";
+// import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+// import { configVariable } from "hardhat/config";
+
+// const config: HardhatUserConfig = {
+//   plugins: [hardhatToolboxMochaEthersPlugin],
+//   solidity: {
+//     profiles: {
+//       default: {
+//         version: "0.8.28",
+//       },
+//       production: {
+//         version: "0.8.28",
+//         settings: {
+//           optimizer: {
+//             enabled: true,
+//             runs: 200,
+//           },
+//         },
+//       },
+//     },
+//   },
+//   networks: {
+//     hardhatMainnet: {
+//       type: "edr-simulated",
+//       chainType: "l1",
+//     },
+//     hardhatOp: {
+//       type: "edr-simulated",
+//       chainType: "op",
+//     },
+//     sepolia: {
+//       type: "http",
+//       chainType: "l1",
+//       url: configVariable("SEPOLIA_RPC_URL"),
+//       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+//     },
+//   },
+// };
+
+// export default config;
+
+
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
-  solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    },
-  },
+  solidity: "0.8.28",
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
+    polygon_amoy: {
+      url: process.env.POLYGON_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 80002, // Polygon Amoy testnet
     },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-    },
+    // polygon_mainnet: {
+    //   url: "https://polygon-rpc.com",
+    //   accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    //   chainId: 137,
+    // },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY || "",
   },
 };
 
